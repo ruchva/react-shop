e7df0ebcd653daeacd38ff298beac12a636ce5cf
ruchva <ruchva@gmail.com>
2022-05-17 20:29:37 -0400

    luego de reparar node

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    mode: 'development',
     resolve: {
         extensions: ['.js','.jsx'],
     },
